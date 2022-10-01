6441555a148756f6040121594fb85dd0cdb7192e
abiyyu03 <abiyyu652@gmail.com>
2022-10-01 17:15:31 +0700

    tidy up app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,18 +9,31 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>LunarTeam</title>
+    <title>Lunar Team Dashboard</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{url('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="{{url('css/sb-admin-2.min.css')}}" rel="stylesheet">
 
+    {{-- javascript --}}
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="{{url('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    {{-- sweetalert2 --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{url('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{url('js/sb-admin-2.min.js')}}"></script>
 </head>
 
 <body id="page-top">
@@ -39,7 +52,6 @@
                 <!-- Begin Page Content -->
                 @yield('content')
                 <!-- /.container-fluid -->
-
             </div>
             <!-- End of Main Content -->
             @stack('scripts')
@@ -84,29 +96,6 @@
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="{{url('vendor/jquery/jquery.min.js')}}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="{{url('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-    {{-- sweetalert2 --}}
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="{{url('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{url('js/sb-admin-2.min.js')}}"></script>
-
-
-    <!-- Page level plugins -->
-    {{-- <script src="{{url('vendor/chart.js/Chart.min.js')}}"></script> --}}
-
-    <!-- Page level custom scripts -->
-    {{-- <script src="{{url('js/demo/chart-area-demo.js')}}"></script>
-    <script src="{{url('js/demo/chart-pie-demo.js')}}"></script> --}}
-
 </body>
 
 </html>
