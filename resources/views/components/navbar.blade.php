e4adba7a16dcb773019ef7ece6c24f77bb33a117
abiyyu03 <abiyyu652@gmail.com>
2022-10-01 15:03:13 +0700

    change avatar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -36,9 +36,10 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth()->guard('isAuthenticated')->user()->username}}</span>
-            <img class="img-profile rounded-circle"
-            src="img/undraw_profile.svg">
+            <span class="mr-2 d-none d-lg-inline text-gray-600">{{Auth()->guard('isAuthenticated')->user()->username}}</span>
+            {{-- <img class="img-profile rounded-circle" 
+            src="img/undraw_profile.svg">--}}
+            <i class="fas fa-user-circle fa-2x"></i>
         </a>
         <!-- Dropdown - User Information -->
         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
