bb9d3796a3ddecea9796dbcea4cbb2bef3da9439
abiyyu03 <abiyyu652@gmail.com>
2022-10-02 07:53:56 +0700

    add plant area table and seeders

---
+++ app/Models/PlantArea.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlantArea extends Model
+{
+    use HasFactory;
+    protected $table = "plant_area";
+    protected $fillable = ['plant_area'];
+}
